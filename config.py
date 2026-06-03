b0945ed83f851841fce5278381de86d6c51a612a
P2SAMAPA <pranabpattanaik@gmail.com>
2026-06-03 09:26:08 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,28 @@
+import os
+
+HF_TOKEN = os.environ.get("HF_TOKEN", "")
+DATA_REPO = "P2SAMAPA/fi-etf-macro-signal-master-data"
+OUTPUT_REPO = "P2SAMAPA/p2-etf-information-field-theory-results"
+
+WINDOWS = [63, 252, 504, 1008, 2016, 4032, 4536]
+
+UNIVERSES = {
+    "FI_COMMODITIES": ["TLT", "VCIT", "LQD", "HYG", "VNQ", "GLD", "SLV"],
+    "EQUITY_SECTORS": [
+        "SPY", "QQQ", "XLK", "XLF", "XLE", "XLV", "XLI", "XLY",
+        "XLP", "XLU", "GDX", "XME", "IWF", "XSD", "XBI", "IWM", "IWD", "IWO"
+    ],
+    "COMBINED": [
+        "TLT", "VCIT", "LQD", "HYG", "VNQ", "GLD", "SLV",
+        "SPY", "QQQ", "XLK", "XLF", "XLE", "XLV", "XLI", "XLY",
+        "XLP", "XLU", "GDX", "XME", "IWF", "XSD", "XBI", "IWM", "IWD", "IWO"
+    ]
+}
+
+# Macro variables to use (subset of available)
+MACRO_VARS = ["VIX", "DXY", "T10Y2Y", "DGS10"]   # can add more
+
+# Wiener filter parameters
+GRAPH_KNN = 5                # number of neighbors for graph construction
+SIGNAL_NOISE_RATIO = 0.1     # prior signal-to-noise ratio (inverse of noise variance)
+TOP_N = 3
